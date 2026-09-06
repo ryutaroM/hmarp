14ad9004950ff632269a1d371e4c6c6bba144714
ryutaroM <arcticmonkey326@gmail.com>
2026-09-06 16:16:10 +0900

    chore: add moonbit plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import moonbit from "vite-plugin-moonbit";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), cloudflare(), moonbit()],
 });
